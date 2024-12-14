6f730e466770511ccaf34481ef2bc5b90c808f86
JheyDev <jennyfer.souzar@gmail.com>
2024-12-14 16:35:25 -0300

    Piano Felipao

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,27 +1,28 @@
-const pianoKeys = document.querySelectorAll(".piano-keys .key");
-const volumeSlider = document.querySelector(".volume-slider input");
-const keysCheck = document.querySelector(".keys-check input");
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const volumeSlider = document.querySelector('.volume-slider input');
+const keysCheck = document.querySelector('.keys-check input');
 
 let mapedKeys = [];
-let audio = new Audio("src/tunes/a.wav");
+let audio = new Audio('src/tunes/a.wav');
 
 const playTune = (key) => {
   audio.src = `src/tunes/${key}.wav`;
   audio.play();
+  // audio.volume = 0.2;
 
-  const clickedKey = document.querySelector(`[data-key="${key}"]`);
-  clickedKey.classList.add("active");
+  const clickedKey = document.querySelector(`[data-key='${key}']`);
+  clickedKey.classList.add('active');
   setTimeout(() => {
-    clickedKey.classList.remove("active");
+    clickedKey.classList.remove('active');
   }, 150);
 };
 
 pianoKeys.forEach((key) => {
-  key.addEventListener("click", () => playTune(key.dataset.key));
+  key.addEventListener('click', () => playTune(key.dataset.key));
   mapedKeys.push(key.dataset.key);
 });
 
-document.addEventListener("keydown", (e) => {
+document.addEventListener('keydown', (e) => {
   if (mapedKeys.includes(e.key)) {
     playTune(e.key);
   }
@@ -32,9 +33,9 @@ const handleVolume = (e) => {
 };
 
 const showHideKeys = () => {
-  pianoKeys.forEach((key) => key.classList.toggle("hide"));
+  pianoKeys.forEach((key) => key.classList.toggle('hide'));
 };
 
-volumeSlider.addEventListener("input", handleVolume);
+volumeSlider.addEventListener('input', handleVolume);
 
-keysCheck.addEventListener("click", showHideKeys);
+keysCheck.addEventListener('click', showHideKeys);
